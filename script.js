4f622baf168760a3c080e0bbafe0ca3f68e481fd
Yenijunior <148855865+Yenijunior@users.noreply.github.com>
2025-02-23 11:46:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const menuIcon = document.querySelector('#menu-icon');
 const navLinks  = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
-  navlinks.classList.toggle('active');
+  navLinks.classList.toggle('active');
 }
